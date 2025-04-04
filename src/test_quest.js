e2767ed69f00874f3fa945d1f4efda952bcb4bf0
marco.mutti@elmec.it <marco.mutti@elmec.it>
2025-04-04 14:46:56 +0200

    fix: add log

--- src/test_quest.js
+++ src/test_quest.js
@@ -4,6 +4,7 @@ import { levelUp } from "@workadventure/quests";
 const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
 
 while(true){
+    console.log("Quest in sleep")
     await sleep(5000)
     await levelUp("TIME_SPENT_IN_THE_GAME", 10);
 }
