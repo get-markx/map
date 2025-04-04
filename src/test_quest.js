6409c19a9d8e8a0dc441cef3194d4daa4b0d1460
marco.mutti@elmec.it <marco.mutti@elmec.it>
2025-04-04 17:29:56 +0200

    fix: change quest

--- src/test_quest.js
+++ src/test_quest.js
@@ -4,8 +4,8 @@ import { levelUp } from "@workadventure/quests";
 const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
 const quest = await getQuest("TIME_SPENT_IN_THE_GAME");
 console.log("Prima del while true")
-while(true){
-    console.log("Quest in sleep")
-    await sleep(5000)
-    await levelUp("TIME_SPENT_IN_THE_GAME", 10);
-}
+
+console.log("Quest in sleep")
+await levelUp("TIME_SPENT_IN_THE_GAME", 10);
+
+
