a4caac3e1dcd33b1f39fe2fca9082cc4ba778f11
marco.mutti@elmec.it <marco.mutti@elmec.it>
2025-04-04 18:04:40 +0200

    fix: fix quest pt 1000

--- src/test_quest.js
+++ src/test_quest.js
@@ -1,11 +1,10 @@
 import { levelUp } from "@workadventure/quests";
 
-
 const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
 const quest = await getQuest("TIME_SPENT_IN_THE_GAME");
 console.log("Prima del while true")
 while(true){
     console.log("Page Open")
-    await levelUp("TIME_SPENT_IN_THE_GAME", 10);
+    levelUp("TIME_SPENT_IN_THE_GAME", 10).catch(e => console.error(e));
     
 }
