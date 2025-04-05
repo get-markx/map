80e2bf340efcf5841d9e28b98b4bd5970c24146f
Luca Canali <luca.canali@elmec.it>
2025-04-05 11:21:02 +0200

    fix: import lib

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 /// <reference types="@workadventure/iframe-api-typings" />
-import { bootstrapExtra } from "@workadventure/scripting-api-extra";
+//import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 console.log('Script started successfully, hello world');
 //# sourceMappingURL=main.js.map
