9f683149dc44cfb1005df2398afd3b1a1dc9ac82
marco.mutti@elmec.it <marco.mutti@elmec.it>
2025-04-10 15:51:41 +0200

    fix: change script

--- src/test_quest.js
+++ src/test_quest.js
@@ -1,14 +1,11 @@
-import {levelUp} from "@workadventure/quests";
-
 console.log("test_quest")
 
 const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
 
 
-
-    console.log("Page Open")
-    sleep(5000)
+console.log("Page Open")
+sleep(5000)
 console.log("Assign 10 xp once")
-    levelUp("TIME_SPENT_IN_THE_GAME", 10).catch(e => console.error(e));
-    
+levelUp("TIME_SPENT_IN_THE_GAME", 10).catch(e => console.error(e));
+
 
