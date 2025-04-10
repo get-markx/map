06898b20502b4bcaa1baba8ca137a58b02eeca90
Luca Canali <luca.canali@elmec.it>
2025-04-10 16:51:55 +0200

    comnmit

--- src/test_quest.js
+++ src/test_quest.js
@@ -1,4 +1,4 @@
-import {levelUp} from "@workadventure/quests";
+//import {levelUp} from "@workadventure/quests";
 
 console.log("test_quest")
 
@@ -8,6 +8,6 @@ const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
 console.log("Page Open")
 sleep(5000)
 console.log("Assign 10 xp once")
-levelUp("TIME_SPENT_IN_THE_GAME", 10).catch(e => console.error(e));
+WA.player.levelUp("TIME_SPENT_IN_THE_GAME", 10).catch(e => console.error(e));
 
 
