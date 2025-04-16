825ff8fa56a8ad9d8054e4d33085bb699da982f3
marco.mutti@elmec.it <marco.mutti@elmec.it>
2025-04-16 10:04:39 +0200

    fix: add retrieve quest

--- src/test_quest.js
+++ src/test_quest.js
@@ -4,7 +4,12 @@ import {levelUp} from "@workadventure/quests";
 
 (async () => {
     await WA.onInit();
+
     console.log('Current player name: ', WA.player.name);
     console.log("Assign 10 xp once")
     levelUp("TIME_SPENT_IN_THE_GAME", 10).catch(e => console.error(e));
+
+
+    const quest =  getQuest("TIME_SPENT_IN_THE_GAME");
+    console.log(quest)
 })();
